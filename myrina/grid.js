71fde77e22f77405cec77c023e2006e32e44edcb
Sabrina <48449995+venusthms@users.noreply.github.com>
2021-03-09 13:35:28 +0100

    Create grid.js

---
+++ myrina/grid.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+var $grid = $(".grid"); $grid.imagesLoaded(function() {
+   $grid.masonry({
+       itemSelector: ".item",
+       columnWidth: 270 /* SEE NOTE */
+   });
+});
+$(".button").click(function(){
+   $(this).addClass("selected");
+   $(".button").not($(this)).removeClass("selected");
+   var ssf = $(this).attr("filter");
+   $(".item." + ssf).show();
+   $(".item").not("." + ssf).hide();
+   $grid.masonry();
+});
+});
